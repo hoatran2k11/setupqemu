0e73f409fef8492b8899789a143a3f15b1a3cc4d
Hòa Trần <HUYT27871@GMAIL.COM>
2025-07-24 03:34:09 +0700

    Update win7sp1.sh

--- win7sp1.sh
+++ win7sp1.sh
@@ -13,11 +13,11 @@ if [ "$lang_choice" == "1" ]; then
     LANG_VIRTIO_NET_NOTE="Bạn sẽ phải tự chuẩn bị file .iso cài driver VirtIO, hãy đổi tên nó thành virtio.iso và đặt vào cùng thư mục với script này nhé!"
     LANG_NET_CARD_TYPE="Card mạng bạn muốn dùng (ví dụ: e1000, rtl8139): "
     LANG_VNC_PORT="Port VNC bạn muốn dùng: "
-    LANG_ISO_PATH="Đường dẫn đến file ISO Windows (ví dụ: /sdcard/Downloads/win7.iso): "
     LANG_VM_NAME="Tên file ổ cứng ảo (ví dụ: win7_disk.qcow2): "
     LANG_INSTALL_QEMU_MSG="Đang cài đặt QEMU và các gói cần thiết..."
     LANG_CONFIG_VM_MSG="Đang cấu hình máy ảo..."
-    LANG_START_VM_MSG="Khởi động máy ảo. Bạn có thể kết nối VNC đến địa chỉ IP của thiết bị Android của bạn với port đã chọn."
+    LANG_QEMU_CMD_SAVED="Lệnh khởi động QEMU đã được lưu vào file seven.sh. Bạn có thể chạy nó bằng lệnh: ./seven.sh"
+    LANG_DOWNLOAD_ISO_MSG="Đang tải file ISO Windows 7..."
 else
     LANG_RAM="Enter RAM for VM in GB: "
     LANG_CPU="Enter number of CPU cores for VM: "
@@ -26,11 +26,11 @@ else
     LANG_VIRTIO_NET_NOTE="You will need to provide the VirtIO driver .iso. Please rename it to virtio.iso and place it in the same directory as this script!"
     LANG_NET_CARD_TYPE="Enter desired network card type (e.g., e1000, rtl8139): "
     LANG_VNC_PORT="Enter VNC port to use: "
-    LANG_ISO_PATH="Path to Windows ISO file (e.g., /sdcard/Downloads/win7.iso): "
     LANG_VM_NAME="Virtual disk filename (e.g., win7_disk.qcow2): "
     LANG_INSTALL_QEMU_MSG="Installing QEMU and necessary packages..."
     LANG_CONFIG_VM_MSG="Configuring virtual machine..."
-    LANG_START_VM_MSG="Starting virtual machine. You can connect via VNC to your Android device's IP address with the chosen port."
+    LANG_QEMU_CMD_SAVED="QEMU boot command saved to seven.sh. You can run it with: ./seven.sh"
+    LANG_DOWNLOAD_ISO_MSG="Downloading Windows 7 ISO file..."
 fi
 
 clear
@@ -44,15 +44,22 @@ pkg install -y qemu-system-x86_64 curl qemu-utils
 
 clear
 
+echo "$LANG_DOWNLOAD_ISO_MSG"
+WINDOWS_ISO_PATH="en_windows_7_with_sp1_x64.iso"
+curl -o "$WINDOWS_ISO_PATH" https://dl.bobpony.com/windows/7/en_windows_7_with_sp1_x64.iso
+
+clear
+
 echo "$LANG_CONFIG_VM_MSG"
 
 read -p "$LANG_RAM" VM_RAM_GB
 read -p "$LANG_CPU" VM_CPU_CORES
 read -p "$LANG_DISK" VM_DISK_GB
 read -p "$LANG_VIRTIO_NET_PROMPT" USE_VIRTIO_NET
-USE_VIRTIO_NET=$(echo "$USE_VIRTIO_NET" | tr '[:upper:]' '[:lower:]') # Chuyển về chữ thường
+USE_VIRTIO_NET=$(echo "$USE_VIRTIO_NET" | tr '[:upper:]' '[:lower:]')
 
 NET_ARGS=""
+VIRTIO_ISO_DRIVE=""
 if [ "$USE_VIRTIO_NET" == "y" ]; then
     NET_ARGS="-netdev user,id=vnet0 -device virtio-net-pci,netdev=vnet0"
     echo "---"
@@ -62,11 +69,9 @@ if [ "$USE_VIRTIO_NET" == "y" ]; then
 else
     read -p "$LANG_NET_CARD_TYPE" NET_CARD_TYPE
     NET_ARGS="-netdev user,id=vnet0 -device ${NET_CARD_TYPE},netdev=vnet0"
-    VIRTIO_ISO_DRIVE=""
 fi
 
 read -p "$LANG_VNC_PORT" VNC_PORT
-read -p "$LANG_ISO_PATH" WINDOWS_ISO_PATH
 read -p "$LANG_VM_NAME" VM_DISK_FILE
 
 VM_RAM_MB=$((VM_RAM_GB * 1024))
@@ -77,19 +82,22 @@ else
     echo "File ổ cứng ảo '$VM_DISK_FILE' đã tồn tại, sẽ sử dụng file này."
 fi
 
-echo "---"
-echo "$LANG_START_VM_MSG"
-echo "Lệnh QEMU được tạo:"
-echo "qemu-system-x86_64 -m ${VM_RAM_MB} -cpu host -smp ${VM_CPU_CORES} -hda ${VM_DISK_FILE} -cdrom ${WINDOWS_ISO_PATH} ${VIRTIO_ISO_DRIVE} ${NET_ARGS} -vnc :${VNC_PORT} -boot d"
-echo "---"
+QEMU_COMMAND="qemu-system-x86_64 \\
+    -m \"${VM_RAM_MB}\" \\
+    -cpu host \\
+    -smp \"${VM_CPU_CORES}\" \\
+    -hda \"${VM_DISK_FILE}\" \\
+    -cdrom \"${WINDOWS_ISO_PATH}\" \\
+    ${VIRTIO_ISO_DRIVE} \\
+    ${NET_ARGS} \\
+    -vnc \":${VNC_PORT}\" \\
+    -boot d"
 
-qemu-system-x86_64 \
-    -m "${VM_RAM_MB}" \
-    -cpu host \
-    -smp "${VM_CPU_CORES}" \
-    -hda "${VM_DISK_FILE}" \
-    -cdrom "${WINDOWS_ISO_PATH}" \
-    ${VIRTIO_ISO_DRIVE} \
-    ${NET_ARGS} \
-    -vnc ":${VNC_PORT}" \
-    -boot d
+echo "#!/data/data/com.termux/files/usr/bin/bash" > seven.sh
+echo "$QEMU_COMMAND" >> seven.sh
+
+chmod +x seven.sh
+
+echo "---"
+echo "$LANG_QEMU_CMD_SAVED"
+echo "---"
